2030ada558fa6e73eb1373e0b712a8c3b04cef31
Antti Reijonen <areijone@gmail.com>
2019-11-10 22:00:12 +0200

    Eureka

---
+++ eureka-server/src/main/java/fi/joufa/safetor/eurekaserver/Application.java
@@ -0,0 +1,17 @@
+package fi.joufa.safetor.eurekaserver;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableAdminServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
