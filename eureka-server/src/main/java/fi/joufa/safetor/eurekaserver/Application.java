65212551eaf5c95088aa2240f96f38b76a6d9a10
Antti <antti.reijonen@combitech.fi>
2019-11-12 10:51:13 +0200

    Eureka configuration

--- eureka-server/src/main/java/fi/joufa/safetor/eurekaserver/Application.java
+++ eureka-server/src/main/java/fi/joufa/safetor/eurekaserver/Application.java
@@ -1,6 +1,6 @@
 package fi.joufa.safetor.eurekaserver;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
--- teams/teams-service/src/main/java/fi/joufa/safetor/team/service/Application.java
+++ teams/teams-service/src/main/java/fi/joufa/safetor/team/service/Application.java
@@ -10,9 +10,11 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableConfigurationProperties({ApplicationProperties.class})
 public class Application implements InitializingBean {
 
